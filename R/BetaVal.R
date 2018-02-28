703c0ce741c1eb8605a81f61a3e896bae728bff8
Vincent Garin <vincent.garin@wur.nl>
2018-02-28 21:39:38 +0100

    Modif 3: Reduced cross-specific effects
    I reduced the cross-specific effects by decreasing the number of QTL effects. Q1: 36 -> 18; Q2: 18 -> 12.
    
    The bi-allelic QTLs have been re-established in the old way they were created with 2 alleles.

--- R/BetaVal.R
+++ R/BetaVal.R
@@ -19,12 +19,10 @@ BetaVal <- function(QTL, QTL_inc){
     if(Qsc == 1){ # all crosses
 
       # Beta <- AlleleSeries(na = dim(Qmat)[2], a = 0.9)
-      Beta <- round(runif(n = dim(Qmat)[2], min = 1, max = 10), 2)
-      Beta_sign <- sample(x = c(-1, 1), size = dim(Qmat)[2], replace = TRUE)
-
-      Beta_list[[i]] <- Beta * Beta_sign
-
-    } else if (Qsc == 2){ # half of the crosses
+      # Beta <- round(runif(n = dim(Qmat)[2], min = 1, max = 10), 2)
+      # Beta_sign <- sample(x = c(-1, 1), size = dim(Qmat)[2], replace = TRUE)
+      #
+      # Beta_list[[i]] <- Beta * Beta_sign
 
       na <- dim(Qmat)[2]
       na_half <- round(na/2)
@@ -33,6 +31,17 @@ BetaVal <- function(QTL, QTL_inc){
       Beta <- round(runif(n = na_half, min = 1, max = 10), 2)
       Beta_sign <- sample(x = c(-1, 1), size = na_half, replace = TRUE)
       Beta <- c(Beta * Beta_sign, rep(0, (na - na_half)))
+      Beta_list[[i]] <- Beta[sample(1:na)]
+
+    } else if (Qsc == 2){ # half of the crosses
+
+      na <- dim(Qmat)[2]
+      na_third <- round(na/3)
+
+      # Beta <- AlleleSeries(na = na_half, a = 0.8)
+      Beta <- round(runif(n = na_third, min = 1, max = 10), 2)
+      Beta_sign <- sample(x = c(-1, 1), size = na_third, replace = TRUE)
+      Beta <- c(Beta * Beta_sign, rep(0, (na - na_third)))
       Beta_list[[i]] <- Beta[sample(1:na)]
 
     } else if (Qsc == 3){ # all parents
@@ -75,20 +84,20 @@ BetaVal <- function(QTL, QTL_inc){
     } else if (Qsc == 7){ # bi-allelic minor allele
 
       Beta <- round(runif(n = 1, min = 1, max = 10), 2)
-      Beta_sign <- sample(x = c(-1, 1), size = 1, replace = TRUE)
+      # Beta_sign <- sample(x = c(-1, 1), size = 1, replace = TRUE)
 
-      # Beta_list[[i]] <- c(-Beta, Beta)
-      Beta_list[[i]] <- Beta * Beta_sign
+      Beta_list[[i]] <- c(-Beta, Beta)
+      # Beta_list[[i]] <- Beta * Beta_sign
 
       ############### c(-0.5, 0.5)[sample[1:2]]
 
     } else if (Qsc == 8){ # bi-allelic minor allele
 
       Beta <- round(runif(n = 1, min = 1, max = 10), 2)
-      Beta_sign <- sample(x = c(-1, 1), size = 1, replace = TRUE)
+      # Beta_sign <- sample(x = c(-1, 1), size = 1, replace = TRUE)
 
-      # Beta_list[[i]] <- c(-Beta, Beta)
-      Beta_list[[i]] <- Beta * Beta_sign
+      Beta_list[[i]] <- c(-Beta, Beta)
+      # Beta_list[[i]] <- Beta * Beta_sign
 
     }
 
